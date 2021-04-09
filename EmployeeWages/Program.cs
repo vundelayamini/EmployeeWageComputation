40fc2e2c15dffc5d6755421a562c3d1d21076812
vundelayamini <vundelayamini@gmail.com>
2021-04-09 15:42:46 +0530

    [yamini]Add: Employee part time wage

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -7,7 +7,8 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             //constants
-            int PRESENT = 1;
+            int FULL_TIME = 1;
+            int PART_TIME = 2;
             int EMP_RATE_PER_HR = 20;
 
             //local variables
@@ -16,18 +17,23 @@ namespace EmployeeWages
 
             //inbuilt class
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
             Console.WriteLine("random value " + empCheck);
             //selection statements
-            if (empCheck == PRESENT)
+            if (empCheck == FULL_TIME)
             {
                 empHrs = 8;
             }
+            else if(empCheck==PART_TIME)
+            {
+                empWage = 4;
+            }
             else
             {
-                empWage = 0;
+                empHrs = 0;
             }
+            //computation
             empWage = EMP_RATE_PER_HR * empHrs;
             Console.WriteLine("Employee wage per day " + empWage);
             Console.ReadKey();
