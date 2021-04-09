16d91e3e9a40f292ad07bec593ec0e0139a48ceb
vundelayamini <vundelayamini@gmail.com>
2021-04-09 12:32:21 +0530

    [yamini]Add: Emploee daily wage

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -6,15 +6,30 @@ namespace EmployeeWages
     {
         static void Main(string[] args)
         {
-            int present = 1;
+            //constants
+            int PRESENT = 1;
+            int EMP_RATE_PER_HR = 20;
 
+            //local variables
+            int empHrs = 0;
+            int empWage = 0;
+
+            //inbuilt class
             Random random = new Random();
             int empCheck = random.Next(0, 2);
+
             Console.WriteLine("random value " + empCheck);
-            if (empCheck == present)
-                Console.WriteLine("Employee is present");
+            //selection statements
+            if (empCheck == PRESENT)
+            {
+                empHrs = 8;
+            }
             else
-                Console.WriteLine("Employee is absent");
+            {
+                empWage = 0;
+            }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee wage per day " + empWage);
             Console.ReadKey();
         }
     }
