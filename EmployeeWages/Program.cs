a12f32d69ed09dc8bf43a67b60051ca7e69e35a9
vundelayamini <vundelayamini@gmail.com>
2021-04-09 12:04:08 +0530

    [yamini]Add: Employee is present or absent

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int present = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            Console.WriteLine("random value " + empCheck);
+            if (empCheck == present)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+            Console.ReadKey();
+        }
+    }
+}
