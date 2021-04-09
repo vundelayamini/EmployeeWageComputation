47e9896e6f3bf5d3107636ea1d3f9e0bb0afe03d
vundelayamini <vundelayamini@gmail.com>
2021-04-09 16:15:44 +0530

    [yamini]Add: Calculating Wages per 20 days

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -4,42 +4,52 @@ namespace EmployeeWages
 {
     class Program
     {
-            //constants
-           public const int FULL_TIME = 1;
-           public const int PART_TIME = 2;
-           public const int EMP_RATE_PER_HR = 20;
+        //constants
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+        public const int EMP_RATE_PER_HR = 20;
+        public const int WORKING_DAYS = 20;
 
         static void main(string[] args)
-        { 
-               //local variables
-               int empHrs = 0;
-               int empWage = 0;
+        {
+            //local variables
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
 
             //inbuilt class
             Random random = new Random();
-            int empCheck = random.Next(0, 3);
-
-            Console.WriteLine("random value " + empCheck);
-            //selection statements
-
-            switch (empCheck)
+            for (int day= 0; day< WORKING_DAYS; day++)
             {
-                case FULL_TIME:
-                empHrs = 8;
-                 break;
+                int empCheck = random.Next(0, 3);
 
-                case PART_TIME:
-                 empHrs = 4;
-                  break;
-             
-                 case default:
-                empHrs = 0;
-                 break;
+               // Console.WriteLine("random value " + empCheck);
+                //selection statements
+
+                switch (empCheck)
+                {
+                    case FULL_TIME:
+                        empHrs = 8;
+                        break;
+
+                    case PART_TIME:
+                        empHrs = 4;
+                        break;
+
+                    case default:
+                        empHrs = 0;
+                        break;
+                }
+
+                //computation
+                empWage = EMP_RATE_PER_HR * empHrs;
+                Console.WriteLine("per day employee wage" + empWage);
+                totalEmpWage += empWage;//totalEmpWage = totalEmpWage + empWage
             }
-            //computation
-           empWage = EMP_RATE_PER_HR* empHrs;
-           Console.WriteLine("Employee wage per day " + empWage);
-            Console.ReadKey();
+            Console.WriteLine("Employee wage for 20 days " + totalempWage);
+            Console.Read();
+        }
+
         
     }
 }
