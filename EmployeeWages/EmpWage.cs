dca8a77faf402dfe396bd10a195766a53c7dca88
vundelayamini <vundelayamini@gmail.com>
2021-04-15 08:50:40 +0530

    [yamini]Add: Employee Wage for multiple companies

---
+++ EmployeeWages/EmpWage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWages
+{
+    class EmpWage
+    {
+
+        //Constants declarations
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        
+        // Calculate employee wage for multiple companies using method 
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            //Variables declarations
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days is : " + totalWorkingDays + " " + "Employee Hours is :" + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total employee wage for companies : " + company + " is :" + totalEmpWage);
+            return totalEmpWage;
+        }
+    }
+}
+    
+
--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -4,52 +4,13 @@ namespace EmployeeWages
 {
     class Program
     {
-        //constants
-        public const int FULL_TIME = 1;
-        public const int PART_TIME = 2;
-        public const int EMP_RATE_PER_HR = 20;
-        public const int WORKING_DAYS = 20;
-        public const int WORKING_HRS = 20;
-
-        static void main(string[] args)
+        static void Main(string[] args)
         {
-            //local variables
-            int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
-
-            //inbuilt class
-            Random random = new Random();
-            while (hrs < MAX_WORKING_HRS && workingDays<= MAX_WORKING_DAYS)
-            {
-                workingDays++;
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case FULL_TIME:
-                        empHrs = 8;
-                        break;
-
-                    case PART_TIME:
-                        empHrs = 4;
-                        break;
-
-                    case default:
-                        empHrs = 0;
-                        break;
-                }
-
-                //computation
-                empWage = EMP_RATE_PER_HR * empHrs;
-                hrs += empHrs;
-                Console.WriteLine("per day employee wage" + empWage);
-                totalEmpWage += empWage;//totalEmpWage = totalEmpWage + empWage
-            }
-            Console.WriteLine("Employee wage for  " + workingdays + "days" totalempWage);
-            Console.WriteLine("working hrs" + hrs);
-            Console.Read();
+            Console.WriteLine("Welome to Employee Wage Program");
+            EmpWage.computeEmpWage("DMart", 20, 2, 10);
+            EmpWage.computeEmpWage("Reliance", 10, 4, 20);
         }
-
-        
     }
 }
+
+  
