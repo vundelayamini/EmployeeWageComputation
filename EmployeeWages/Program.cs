f0a65318f2c7e1efb9dad4d7c33fe4ad936f3d22
vundelayamini <vundelayamini@gmail.com>
2021-04-15 13:01:12 +0530

    [yamini]Add: Save the total wage by company

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -2,56 +2,19 @@
 
 namespace EmployeeWages
 {
-    class Program
+    public class Program
     {
-        //constants
-        public const int FULL_TIME = 1;
-        public const int PART_TIME = 2;
-        public const int EMP_RATE_PER_HR = 20;
-        public const int MAX_WORKING_DAYS = 20;
-        public const int MAX_WORKING_HRS = 10;
-
-        public static int ComputeEmpWage()
-        {
-            //local variables
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-
-            //inbuilt class
-            while (totalEmpHrs < MAX_WORKING_HRS && totalWorkingDays <= MAX_WORKING_DAYS)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case FULL_TIME:
-                        empHrs = 8;
-                        break;
-
-                    case PART_TIME:
-                        empHrs = 4;
-                        break;
-
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-
-                // computation
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Days:" + totalWorkingDays + "Emp Hrs:" + empHrs);
-            }
-               int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
-            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
-            return totalEmpWage;
-
-        }
         static void Main(string[] args)
         {
-            ComputeEmpWage();
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeEmpwage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpwage();
+            Console.WriteLine(reliance.toString());
         }
+          
     }
-
 
 
 
